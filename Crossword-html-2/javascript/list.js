08676d5b22ef50a3b48471b32ada4337140fb073
mr-yeung <92673431+mr-yeung@users.noreply.github.com>
2022-02-20 20:58:53 +1100

    Add files via upload

---
+++ Crossword-html-2/javascript/list.js
@@ -0,0 +1,12 @@
+var puzzlewords = [
+['dmVsb2NpdHk=', 'the speed of something in a particular direction'],
+['b2Zmc2hvcmU=', 'happening or existing in the sea, not far from the land'],
+['YW5jaG9y', 'to fix something in position so that it cannot move'],
+['bWV0ZW9yb2xvZ3k=', 'the scientific study of the earth’s atmosphere and its changes, used especially in predicting what the weather will be like'],
+['cGlsZQ==', 'a strong column or post of wood, metal, or concrete that is pushed into the ground to help support a building'],
+['YnJvd3Nlcg==', 'a computer program that lets you look at or read documents on the World Wide Web'],
+['aGFyc2g=', '(of weather or living conditions) very difficult and unpleasant to live in'],
+['dHVyYmluZQ==', 'a machine or part of a machine that produces continuous turning power from a fast-moving flow of a liquid or gas, using a set of vanes attached to a wheel or rotor'],
+['b3B0aWNhbA==', '(computing) using light for reading or storing information'],
+];
+crosswordPuzzle(puzzlewords);
