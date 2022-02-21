715b84ea6c03ad529b9e32cbe60b9d221df3377b
mr-yeung <92673431+mr-yeung@users.noreply.github.com>
2022-02-21 18:08:34 +1100

    Add files via upload

---
+++ Crossword-ct-2/javascript/list.js
@@ -0,0 +1,12 @@
+var puzzlewords = [
+['cHJvZ3JhbQ==', '(verb) To give a computer, etc. a set of instructions to make it perform a particular task'],
+['c2tldGNo', '(noun) This is the name Arduino give their code or programs'],
+['VVNC', '(noun) The system for connecting other pieces of equipment to a computer: Abbr.'],
+['Y29tcGF0aWJsZQ==', '(adj) (of machines, especially computers, or software) able to be used together'],
+['VGhpbmtlcnNoaWVsZA==', '(noun) A board which plugs in to a standard Arduino board and contains a variety of on-board components for learning coding without need to learn electronics'],
+['QXJkdWlubw==', '(noun) A small and easy to use computer board. A programmable microcontroller board'],
+['Ymxpbms=', '(verb) to shine with an unsteady light; to flash on and off'],
+['dXBsb2Fk', '(verb) to send data to another computer'],
+['c3ludGF4', '(noun) (computing) the rules that state how words and phrases must be used in a computer language']
+];
+crosswordPuzzle(puzzlewords);
