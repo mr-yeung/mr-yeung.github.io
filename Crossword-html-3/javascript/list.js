20373e9f9a59cbdae61342ecfe3ce51b764208d3
mr-yeung <92673431+mr-yeung@users.noreply.github.com>
2022-03-08 07:52:06 +1100

    Add files via upload

---
+++ Crossword-html-3/javascript/list.js
@@ -0,0 +1,14 @@
+var puzzlewords = [
+['ZWxlY3Ryb25pY2FsbHk=', '(adverb) using a device that works in an electronic way'],
+['ZXhwbGFuYXRpb24=', '(noun) a reason given for something'],
+['am91cm5leQ==', '(noun) an act of travelling from one place to another'],
+['cmVzZWFyY2hlcg==', '(noun) a person who studies something carefully and tries to discover new facts about it'],
+['YXJjaGl0ZWN0dXJl', '(noun) the design or style of a building or buildings'],
+['YnV0dG9u', '(noun) a small area on a computer screen that you click on to make it do something'],
+['R292ZXJub3I=', '(noun) a person who is the official head of a country or region that is governed by another country'],
+['bGF1bmNo', '(verb) to start a computer program'],
+['aW5kZXg=', '(noun) a list of names or topics that are referred to in a book'],
+['aW1hZ2U=', '(noun) a picture of somebody/something seen on a television, computer, phone'],
+['dW5kZXJsaW5l', '(verb) to draw or print a line under a word, sentence']
+];
+crosswordPuzzle(puzzlewords);
