42d2f116ded862df11cd9152a93f6236361d46b8
mr-yeung <92673431+mr-yeung@users.noreply.github.com>
2022-03-08 20:08:04 +1100

    Add files via upload

---
+++ Crossword-mlm-2/javascript/list.js
@@ -0,0 +1,15 @@
+var puzzlewords = [
+['YmluYXJ5', '(noun) a system of numbers that uses only 0 and 1'],
+['Yml0bWFw', '(noun) a way in which an image is stored with a fixed number of bits (= units of information) for each unit of the image'],
+['c3RvcmFnZQ==', '(noun) the process of keeping information, etc. on a computer'],
+['cGl4ZWw=', '(noun) the smallest element of an image that can be individually processed in a video display system'],
+['UGhvdG9zaG9w', '(noun) the proprietary name of a brand of computer software used to digitally alter digital photographs or other graphics'],
+['Yml0cw==', '(noun) Also called binary digit. a single, basic unit of information, used in connection with computers and information theory.'],
+['bWVtb3J5', '(noun) device or system that is used to store information for immediate use in a computer'],
+['ZG93bmxvYWQ=', '(verb) to get data from another computer, usually using the internet'],
+['Y29tcHJlc3Npb24=', '(noun) the act of making computer files, etc. smaller so that they use less space on a disk, etc.'],
+['c2l6ZQ==', '(noun) how large or small a person or thing is'],
+['cGhvdG9ncmFwaGlj', '(adj) connected with photographs or photography'],
+['d2Vi', '(noun) a system for finding information on the internet, in which documents are connected to other documents']
+];
+crosswordPuzzle(puzzlewords);
